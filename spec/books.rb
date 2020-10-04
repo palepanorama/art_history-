92de2d33cc8419a4ef1bbb31ad737f26f7d50c2b
Matthew Thomas <owensite@Matthews-Air.attlocal.net>
2020-10-04 14:38:45 -0500

    removes several unnecessary files

--- spec/books.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Books do
-  it "has a version number" do
-    expect(Books::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
