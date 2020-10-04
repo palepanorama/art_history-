84090004cb8182d49f54978becf855a61167982f
Matthew Thomas <owensite@Matthews-Air.attlocal.net>
2020-10-04 14:32:42 -0500

    removes boldface font from some of the interface to improve readability

--- lib/cli.rb
+++ lib/cli.rb
@@ -17,9 +17,9 @@ class CLI
             elsif input.to_i > 0 && input.to_i <= Book.find_by_query(@queries).length
                 @book = Book.find_by_query(@queries)[input.to_i - 1]
                 print_choice(@book)
-                puts "If you'd like to see information on another book from this list, type its number in here.".blue.bold
+                puts "If you'd like to see information on another book from this list, type its number in here.".blue
                 puts ""
-                puts "To see the list again, type 'books.'".blue.bold
+                puts "To see the list again, type 'books.'".blue
             else 
                 puts "I don't understand that. Please try again.".blue.bold
             end  
@@ -32,13 +32,13 @@ class CLI
         puts "Welcome to Matt's Book Finder!".blue.bold
         puts "-----------------------------".blue.bold
         puts ""
-        puts "Enter a keyword to search for or type 'exit' to exit".blue.bold
+        puts "Enter a keyword to search for or type 'exit' to exit".blue
         puts ""
     end 
     
     def prompt
         puts ""
-        puts "Select a number to see the details of a book.".blue.bold
+        puts "Select a number to see the details of a book.".blue
         puts ""
     end 
 
@@ -46,21 +46,21 @@ class CLI
     def print_books
         results = Book.find_by_query(@queries)
         results.each.with_index(1) do |b, i|
-            puts "#{i}. #{b.title}".blue.bold
+            puts "#{i}. #{b.title}".blue
         end 
         prompt 
     end 
 
     def print_choice(book)
-        puts "Title: #{book.title}".blue.bold
+        puts "Title: #{book.title}".blue.bold 
         puts ""
-        puts "Author: #{book.author[0]}".blue.bold if book.author != nil 
+        puts "Author: #{book.author[0]}".blue if book.author != nil 
         puts ""
-        puts "ISBN: #{book.isbn[0]}".blue.bold if book.isbn != nil 
+        puts "ISBN: #{book.isbn[0]}".blue if book.isbn != nil 
         puts ""
-        puts "Year published: #{book.publish_year[0]}".blue.bold if book.publish_year != nil 
+        puts "Year published: #{book.publish_year[0]}".blue if book.publish_year != nil 
         puts ""
-        puts "Subject: #{book.subject}".blue.bold if book.subject != nil 
+        puts "Subject: #{book.subject}".blue if book.subject != nil 
         puts ""
     end 
 
@@ -69,7 +69,7 @@ class CLI
         puts "Thanks for using Matt's Book Finder!".blue.bold
         puts "-----------------------------------".blue.bold
         puts ""
-        puts "Happy reading!".blue.bold
+        puts "Happy reading!".blue
         puts "--------------".blue.bold
         puts ""
     end 
